14f36038573b8b4bcc091008612535309f3b7b6c
lIMpY <xxdilbertxx@live.com>
2020-12-16 21:24:53 -0500

    Update

--- MyFirstApp.py
+++ MyFirstApp.py
@@ -16,9 +16,12 @@ import sys
 import os
 def wipe(): os.system('clear')
 
-from myconfig import *
-lighttimer = 120
-adminrole = 'Test1'
+from config import *
+# \/ In config.py
+#TOKEN = open("/home/pi/Discord/token.txt","r").readline()
+#lighttimer
+#adminrole
+# --
 
 pir1 = 11
 pir2 = 12
@@ -33,8 +36,6 @@ GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(pir1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-TOKEN = open("/home/pi/Discord/token.txt","r").readline()
 bot = commands.Bot(command_prefix='.')
 
 @bot.event
@@ -88,7 +89,6 @@ async def hide(ctx, amount=3) :
 async def hide_error(ctx, error):
     if isinstance(error, commands.CheckFailure):
         print(f'{ctx.message.author.name} Error {error} At  {time.asctime(time.localtime(time.time()))}')
-
 
 @bot.group()
 async def led(ctx):
@@ -152,7 +152,6 @@ async def background_task():
         #await on_exit()
 
 #bot.loop.create_task(background_task())
-#print('')
 
 """
 alarm_time = '02:30' #24hrs
@@ -188,7 +187,62 @@ async def time_check():
         s.run() # run the scheduler, will block the event loop
 bot.loop.create_task(time_check())
 """
+"""
+@bot.listen("on_error")
+async def on_command_error(self, ctx, error):
+    # if command has local error handler, return
+    if hasattr(ctx.command, 'on_error'):
+        return
+    # get the original exception
+    error = getattr(error, 'original', error)
 
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+    if isinstance(error, commands.BotMissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'I need the **{}** permission(s) to run this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.DisabledCommand):
+        await ctx.send('This command has been disabled.')
+        return
+
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("This command is on cooldown, please retry in {}s.".format(math.ceil(error.retry_after)))
+        return
+
+    if isinstance(error, commands.MissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.UserInputError):
+        await ctx.send("Invalid input.")
+        await self.send_command_help(ctx)
+        return
+
+    if isinstance(error, commands.NoPrivateMessage):
+        try:
+            await ctx.author.send('This command cannot be used in direct messages.')
+        except discord.Forbidden:
+            pass
+        return
+
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You do not have permission to use this command.")
+        return
+"""
 async def quit_exit():
     print()
     print('Exiting RPi Bot.')
